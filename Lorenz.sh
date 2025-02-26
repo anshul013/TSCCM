63c74c49f03a6f8bf8952bd06289b930b435c706
Anshul <44233149+anshul013@users.noreply.github.com>
2025-02-26 20:00:07 +0100

    Add files via upload

---
+++ Lorenz.sh
@@ -0,0 +1,119 @@
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/LongForecasting" ]; then
+    mkdir ./logs/LongForecasting
+fi
+
+if [ ! -d "./logs/LongForecasting/TSMixerC" ]; then
+    mkdir ./logs/LongForecasting/TSMixerC
+fi
+seq_len=336
+model_name=TSMixerC
+dataset=Lorenz
+num_channels=3
+
+#Best configuration for ETTm2 and 96 frames horizon
+pred_len=96
+python3 -u run_longExp.py \
+  --activation 'relu' \
+  --dropout 0.9 \
+  --beta 0.3 \
+  --n_layers 4 \
+  --d_ff 64 \
+  --cluster_ratio 0.3 \
+  --individual "c" \
+  --data_dim $num_channels \
+  --is_training 1 \
+  --root_path ./dataset/ \
+  --data_path $dataset.csv \
+  --model_id $dataset'_'$seq_len'_'$pred_len \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len $seq_len \
+  --pred_len $pred_len \
+  --enc_in $num_channels \
+  --des 'Exp' \
+  --patience 5 \
+  --train_epochs 100 \
+  --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
+
+#Best configuration for ETTm2 and 192 frames horizon
+pred_len=192
+python3 -u run_longExp.py \
+  --activation 'relu' \
+  --dropout 0.9 \
+  --beta 0.3 \
+  --n_layers 4 \
+  --d_ff 64 \
+  --cluster_ratio 0.3 \
+  --individual "c" \
+  --data_dim $num_channels \
+  --is_training 1 \
+  --root_path ./dataset/ \
+  --data_path $dataset.csv \
+  --model_id $dataset'_'$seq_len'_'$pred_len \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len $seq_len \
+  --pred_len $pred_len \
+  --enc_in $num_channels \
+  --des 'Exp' \
+  --patience 5 \
+  --train_epochs 100 \
+  --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
+
+#Best configuration for ETTm2 and 336 frames horizon
+pred_len=336
+python3 -u run_longExp.py \
+  --activation 'relu' \
+  --dropout 0.9 \
+  --beta 0.3 \
+  --n_layers 4 \
+  --d_ff 128 \
+  --cluster_ratio 0.3 \
+  --individual "c" \
+  --data_dim $num_channels \
+  --is_training 1 \
+  --root_path ./dataset/ \
+  --data_path $dataset.csv \
+  --model_id $dataset'_'$seq_len'_'$pred_len \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len $seq_len \
+  --pred_len $pred_len \
+  --enc_in $num_channels \
+  --des 'Exp' \
+  --patience 5 \
+  --train_epochs 100 \
+  --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
+
+#Best configuration for ETTm2 and 720 frames horizon
+pred_len=720
+python3 -u run_longExp.py \
+  --activation 'relu' \
+  --dropout 0.9 \
+  --beta 0.3 \
+  --n_layers 4 \
+  --d_ff 128 \
+  --cluster_ratio 0.3 \
+  --individual "c" \
+  --data_dim $num_channels \
+  --is_training 1 \
+  --root_path ./dataset/ \
+  --data_path $dataset.csv \
+  --model_id $dataset'_'$seq_len'_'$pred_len \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len $seq_len \
+  --pred_len $pred_len \
+  --enc_in $num_channels \
+  --des 'Exp' \
+  --patience 5 \
+  --train_epochs 100 \
+  --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
